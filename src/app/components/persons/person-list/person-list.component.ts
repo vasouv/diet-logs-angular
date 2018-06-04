794a23dc4a935e6b33c2d90fe787c6b34f6ae986
Souvatzis Vasilios <vasouv@gmail.com>
2018-06-04 14:20:29 +0300

    basic UI for person list

--- src/app/components/persons/person-list/person-list.component.ts
+++ src/app/components/persons/person-list/person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from '../../../models/Person';
 
 @Component({
   selector: 'app-person-list',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonListComponent implements OnInit {
 
+  persons: Person[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    let vasouv = new Person(1, "Vasilis", "Themistokleous", "72525", 31, 1.77, 90, "cholesterol", "anti-cholesterol pills", "sometimes pollen");
+    let chris = new Person(2, "Christos", "Sardewn", "12345", 31, 1.80, 78, "none", "nome", "none");
+    let maik = new Person(3,"Mixalis","Paulou Mela","23456",30,1.82,95.3,"none","cough syrup","a lot");
+    this.persons.push(vasouv);
+    this.persons.push(chris);
+    this.persons.push(maik);
   }
 
 }
--- src/app/models/Person.ts
+++ src/app/models/Person.ts
@@ -1,4 +1,4 @@
-export interface Person {
+export class Person {
     id: number;
     name: string;
     address: string;
@@ -9,4 +9,19 @@ export interface Person {
     illness: string;
     medication: string;
     allergies: string;
+
+    constructor(id: number, name: string, address: string, phone: string, age: number,
+        height: number, targetKg: number, illness: string, medication: string, allergies: string) {
+        this.address = address;
+        this.age = age;
+        this.allergies = allergies;
+        this.height = height;
+        this.id = id;
+        this.illness = illness;
+        this.medication = medication;
+        this.name = name;
+        this.phone = phone;
+        this.targetKg = targetKg;
+    }
+
 }
