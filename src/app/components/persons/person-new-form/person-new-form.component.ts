f47546c0378ebd7255acdeba0fa842918b35326b
Souvatzis Vasilios <vasouv@gmail.com>
2018-06-10 12:33:56 +0300

    saves person to backend
    no validation yet

--- src/app/components/persons/person-new-form/person-new-form.component.ts
+++ src/app/components/persons/person-new-form/person-new-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from '../../../models/Person';
+import { PersonService } from '../../../services/person.service';
 
 @Component({
   selector: 'app-person-new-form',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonNewFormComponent implements OnInit {
 
-  constructor() { }
+  person: Person = {
+    id: 0,
+    name: '',
+    address: '',
+    phone: '',
+    age: 0,
+    height: 0.0,
+    targetkg: 0.0,
+    illness: '',
+    medication: '',
+    allergies: ''
+  };
+
+  constructor(private personService: PersonService) { }
 
   ngOnInit() {
+  }
+
+  addPerson(): void {
+    this.personService.savePerson(this.person).subscribe(savedPerson => console.log(savedPerson));
+    this.clear();
+  }
+
+  clear(): void {
+    this.person = {
+      id: 0,
+      name: '',
+      address: '',
+      phone: '',
+      age: 0,
+      height: 0.0,
+      targetkg: 0.0,
+      illness: '',
+      medication: '',
+      allergies: ''
+    }
   }
 
 }
--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from "rxjs/Observable";
 import { Person } from '../models/Person';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
 
 @Injectable()
 export class PersonService {
@@ -12,6 +16,10 @@ export class PersonService {
 
   public getPersons(): Observable<Person[]> {
     return this.httpClient.get<Person[]>(this.personsUrl);
+  }
+
+  public savePerson(person: Person): Observable<Person> {
+    return this.httpClient.post<Person>(this.personsUrl, person, httpOptions);
   }
 
 }
