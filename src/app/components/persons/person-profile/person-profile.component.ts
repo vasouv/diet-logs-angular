334c075111126852ed0b5992bc6cdb8818a15035
Souvatzis Vasilios <vasouv@gmail.com>
2018-06-15 23:07:25 +0300

    created person profile files

---
+++ src/app/components/persons/person-profile/person-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-profile',
+  templateUrl: './person-profile.component.html',
+  styleUrls: ['./person-profile.component.css']
+})
+export class PersonProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
